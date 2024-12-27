2348335ebda98773592ef36722ddb8461df98446
ayeniayodeji2009 <ayeniayodeji2009@gmail.com>
2024-12-27 04:43:46 +0100

    Restor previous files

---
+++ wsgi.py
@@ -0,0 +1,13 @@
+import os
+from flask import Flask, request, jsonify
+from flask_cors import CORS
+import sqlite3
+from werkzeug.security import generate_password_hash, check_password_hash
+from datetime import datetime
+
+app = Flask(__name__)
+CORS(app, resources={r"/*": {"origins": "*"}})
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 4000))
+    app.run(host="0.0.0.0", port=port)  # Pass the variable `port` directly
